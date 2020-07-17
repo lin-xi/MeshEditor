2e55a179d6500f65e09c5377f39b1e420f2cb337
lin-xi <mcnjqe@163.com>
2020-07-17 20:47:43 +0800

    update

---
+++ src/components/editor/ToolBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tool-bar"></div>
+</template>
+
+<script lang="javascript">
+export default {
+  name: "ToolBar"
+}
+</script>
+
+<style scoped lang="less">
+.tool-bar {
+  background-color: #333;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/Edit.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="editor">
+    <div class="editor-toolbar">
+      <ToolBar />
+    </div>
+    <div class="editor-main">
+      <Split style="height: 2000px;" :gutterSize="5">
+        <SplitPanel :size="80">
+          <Editor />
+        </SplitPanel>
+        <SplitPanel :size="20"></SplitPanel>
+      </Split>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ToolBar from "@/components/editor/ToolBar.vue";
+import Editor from "@/components/editor/Editor.vue";
+import Split from "@/components/common/Split/Split.vue";
+import SplitPanel from "@/components/common/Split/SplitPanel.vue";
+
+export default {
+  name: "Edit",
+  components: {
+    ToolBar,
+    Editor,
+    Split,
+    SplitPanel
+  }
+};
+</script>
+
+<style scoped lang="less">
+.editor {
+  background-color: #444;
+  .editor-toolbar {
+    height: 40px;
+  }
+}
+</style>
